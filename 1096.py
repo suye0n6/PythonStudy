1db47cd935a665da4e1041f6707ea7d5500da291
suyeon <acnm86@gmail.com>
2023-09-12 12:14:45 +0900

    6096를 딕셔너리로

--- 1096.py
+++ 1096.py
@@ -5,7 +5,7 @@ for i in range(20): #좌표가 1~19까지니까 20까지 배열 돌려주기
     location.append([]) #좌표까지 돌리면서 배열안에 값 삽입하기
     
     for j in range(20): #20까지 배열 돌려주기
-        location[i].append(0) #배열 i를 값에 넣어주기
+        location[i].append(0) #0으로 깔기
 
 for i in range(n):
     x, y = map(int, input().split()) ##x,y 좌표값 넣어주기
---
+++ d6069.py
@@ -0,0 +1,3 @@
+score = input()
+a = {'A' : 'best!!!', 'B' : 'good!!', 'C' : 'run!', 'D' : "slowly~"}
+print(a.get(score, 'what?'))
